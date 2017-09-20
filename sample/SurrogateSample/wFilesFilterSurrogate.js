67f316be716c48d0a5eed07b72e217ee4ea35b70
volodymyrmigdal <volodymyr.migdal@gmail.com>
2017-09-20 16:42:02 +0300

    rm temp dir

--- sample/SurrogateSample/wFilesFilterSurrogate.js
+++
@@ -1,8 +0,0 @@
-var rootPath = "/Users/fov/Documents/GitHub/wFilesFilterSurrogate/sample"
-var wFilesTree = 
-{
-  "Filter.Surrogate.filesTreeMake.js" : "", 
-  "Filter.Surrogate.html" : "", 
-  "Filter.Surrogate.js" : "", 
-  "README.md" : ""
-}
